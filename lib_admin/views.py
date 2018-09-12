56c9d43f31bc2abda9e0426d8c43000521b45b55
eashwerks <eashwerks@gmail.com>
2018-09-12 23:40:58 +0530

    6th

--- lib_admin/views.py
+++ lib_admin/views.py
@@ -4,5 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 
 # Create your views here.
-def lib(request):
+def home(request):
     return render(request,'Lms_HomePage.html',{})
+def about(request):
+    return render(request,'About.html',{})
