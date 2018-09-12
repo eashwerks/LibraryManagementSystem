c25cd4ddb4b96906577e0e701a7a6e7a8f63b03c
eashwerks <eashwerks@gmail.com>
2018-09-12 17:53:47 +0530

    5th

--- lib_admin/urls.py
+++ lib_admin/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url,include
 from lib_admin.views import *
 
 urlpatterns = [
-    url(r'^lib/',lib,name='lib'),
+    url(r'^lib1/',lib,name='lib'),
 ]
--- lib_admin/views.py
+++ lib_admin/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def lib(request):
-    return render(request,'lib_admin.html',{})
+    return render(request,'Lms_HomePage.html',{})
